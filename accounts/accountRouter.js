bbd6cc3e2539b0f5da27da6639aca890c7fe4ab4
sooshe <sooshe@sooshebot.com>
2020-02-17 15:47:34 -0600

    POST req success

--- accounts/accountRouter.js
+++ accounts/accountRouter.js
@@ -6,40 +6,71 @@ const Account = require("./accountDb");
 
 const router = express.Router();
 
-router.get("/", (req,res) => {
-    Account.get(req.query)
+router.get("/", (req, res) => {
+  Account.get(req.query)
     .then(account => {
-        res.status(200).json(account);
+      res.status(200).json(account);
     })
     .catch(err => {
-        console.log(err);
-        res.status(500).json({ errorMessage: "Could not retrieve accounts database."})
+      console.log(err);
+      res
+        .status(500)
+        .json({ errorMessage: "Could not retrieve accounts database." });
     });
 });
 
-router.get("/:id", validateAccountId, (req,res) => {
-    const { id } = req.params;
-    Account.getById(id)
+router.get("/:id", validateAccountId, (req, res) => {
+  const { id } = req.params;
+  Account.getById(id)
     .then(account => {
-        res.status(200).json(account);
+      res.status(200).json(account);
     })
     .catch(err => {
-        console.log(err);
-        res.status(500).json({ errorMessage: "Could not retrieve account in db with that id."})
+      console.log(err);
+      res
+        .status(500)
+        .json({
+          errorMessage: "Could not retrieve account in db with that id."
+        });
     });
-})
+});
+
+router.post("/", validateAccount, (req,res) => {
+    const accounts = { ...req.body};
+    Account.insert(accounts)
+    .then(account => {
+        res.status(201).json(account);
+      })
+      .catch(err => {
+        console.log(err);
+        res
+          .status(500)
+          .json({
+            errorMessage: "Could not post to this account."
+          });
+      });
+});
 
 //middleware
 function validateAccountId(req, res, next) {
-    // do your magic!
-    Account.getById(req.params.id)
-    .then(account => {
-      if (account) {
-        req.account = account;
+  // do your magic!
+  Account.getById(req.params.id).then(account => {
+    if (account) {
+      req.account = account;
+      next();
+    } else {
+      res.status(400).json({ errorMessage: "invalid account ID" });
+    }
+  });
+};
+
+function validateAccount(req,res,next) {
+    if (req.body.name && req.body.budget) {
         next();
-      } else {
-        res.status(400).json({ errorMessage: "invalid account ID" });
-      }
-    });
-  };
+    } else {
+        res.status(400).json({ error: "Please add name and budget"});
+    }
+};
+
+
 module.exports = router;
