c52b2216a7939ed0d7b0d29b52ca0b117b7a698f
sooshe <sooshe@sooshebot.com>
2020-02-17 15:17:24 -0600

    server test pass, debug GET /api/accounts

--- accounts/accountRouter.js
+++ accounts/accountRouter.js
@@ -1,7 +1,33 @@
 const express = require("express");
 
-const db = require("./accountDb");
+const db = require("../data/dbConfig");
+
+const Account = require("./accountDb");
 
 const router = express.Router();
 
+router.get("/", (req,res) => {
+    Account.get(req.query)
+    .then(account => {
+        res.status(200).json(account);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({ errorMessage: "Could not retrieve accounts database."})
+    });
+});
+
+//middleware
+function validateAccountId(req, res, next) {
+    // do your magic!
+    Account.getById(req.params.id)
+    .then(account => {
+      if (account) {
+        req.account = account;
+        next();
+      } else {
+        res.status(400).json({ errorMessage: "invalid account ID" });
+      }
+    });
+  };
 module.exports = router;
