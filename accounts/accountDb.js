e5a63d0e24ec3a5aa70c7e81ba9320ff8626164d
sooshe <sooshe@sooshebot.com>
2020-02-17 14:56:13 -0600

    write get and insert functions in accountdb file

--- accounts/accountDb.js
+++ accounts/accountDb.js
@@ -1,10 +1,28 @@
 const db = require("../data/dbConfig");
 
 module.exports = {
-    get,
-    getById,
-    insert,
-    update,
-    remove
+  get,
+  getById,
+  insert,
+  update,
+  remove
 };
+
+function get() {
+  return db("accounts");
+}
+
+function getById(id) {
+  return db("accounts")
+    .where({ id })
+    .first();
+}
+
+function insert(account) {
+    return db("accounts")
+    .insert(account)
+    .then(ids => {
+      return getById(ids[0]);
+    });
+}
 
