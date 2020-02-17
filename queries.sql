5352527416d2195721e6a951ab4caabc3347c50f
sooshe <sooshe@sooshebot.com>
2020-02-17 16:52:14 -0600

    write queries in queries.sql

--- queries.sql
+++ queries.sql
@@ -1,12 +1,44 @@
 -- Database Queries
 
 -- Find all customers with postal code 1010
+SELECT 
+    * 
+FROM 
+    [Customers]
+WHERE 
+    [PostalCode] = 1010;
+    --returns 3 records
 
 -- Find the phone number for the supplier with the id 11
+SELECT 
+    * 
+FROM 
+    [Suppliers] 
+WHERE 
+    [SupplierId] = 11;
+    --Phone number: (010) 9984510
 
 -- List first 10 orders placed, sorted descending by the order date
+SELECT 
+    * 
+FROM 
+    [Orders] 
+ORDER BY 
+    [OrderDate] DESC 
+--LIMIT 10; [getting an error when this is uncommented whyyyyyy???????]
 
 -- Find all customers that live in London, Madrid, or Brazil
+SELECT 
+    *
+FROM 
+    [Customers] 
+WHERE 
+    [City] 
+IN 
+    ("London","Madrid")
+OR
+    [Country] = "Brazil"
+
 
 -- Add a customer record for "The Shire", the contact name is "Bilbo Baggins" the address is -"1 Hobbit-Hole" in "Bag End", postal code "111" and the country is "Middle Earth"
 
